9c5a10644dc66698d0b797a0430da2aab326370f
arkupiec <artur.kupiec@dbschenker.com>
2020-10-24 23:37:43 +0200

    fix hrefs & filters

--- finder_web/src/bazarek-search/BazarekFilters/BazarekFilters.js
+++ finder_web/src/bazarek-search/BazarekFilters/BazarekFilters.js
@@ -3,7 +3,18 @@ import React from 'react';
 import AutoComplete from 'react-select';
 
 
-function AutoSelectFilter({ options, name, onChange, and, onAndChange }) {
+function AutoSelectFilter({ options, name, onChange }) {
+  const onChangeValues = (opts) => onChange(opts?.map(o => o.value) || [])
+
+  return (<div className={'input-group filter-' + name}>
+    <div className="input-group-prepend">
+      <label className="input-group-text">{name}</label>
+    </div>
+    <AutoComplete className="my-select-class" options={options} isMulti name={name} onChange={onChangeValues} />
+  </div>);
+}
+
+function AutoSelectAndFilter({ options, name, onChange, and, onAndChange }) {
   const onChangeValues = (opts) => onChange(opts?.map(o => o.value) || [])
 
   let btnClassName = 'btn btn-outline-secondary';
@@ -42,9 +53,9 @@ export function BazarekFilters({ pickers, filters, setFilters }) {
 
   const onChange = (name) => ev => setFilters({ ...filters, [name]: ev });
   return <div className="grid-filters">
-    <AutoSelectFilter onChange={onChange('category')} options={categories} and={filters.categoriesAnd} onAndChange={onChange('categoriesAnd')} name="Categories" />
-    <AutoSelectFilter onChange={onChange('review')} options={reviews} and={filters.reviewsAnd} onAndChange={onChange('reviewsAnd')} name="Reviews" />
-    <AutoSelectFilter onChange={onChange('tag')} options={tags} and={filters.tagsAnd} onAndChange={onChange('tagsAnd')} name="Tags" />
+    <AutoSelectAndFilter onChange={onChange('category')} options={categories} and={filters.categoriesAnd} onAndChange={onChange('categoriesAnd')} name="Categories" />
+    <AutoSelectFilter onChange={onChange('review')} options={reviews} name="Reviews" />
+    <AutoSelectAndFilter onChange={onChange('tag')} options={tags} and={filters.tagsAnd} onAndChange={onChange('tagsAnd')} name="Tags" />
     <BasicInput name="Reviews_Count" value={filters.reviewsCount} onChange={onChange('reviewsCount')} />
     <BasicInput name="Max Price" value={filters.price} onChange={onChange('price')} />
     <BasicInput name="Limit" value={filters.limit} onChange={onChange('limit')} />
--- finder_web/src/bazarek-search/GenericTable/CellRenderers/HrefCellRenderer.js
+++ finder_web/src/bazarek-search/GenericTable/CellRenderers/HrefCellRenderer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const HrefCellRenderer = (rowData) => {
   return <div className="hrefs">
-    <a href={rowData.Href} target="_blank"><i className="ico steam-ico" /></a>
-    <a href={rowData.Bazarek?.Href} target="_blank"><i className="ico bazarek-ico" /></a>
+    <a href={rowData.Steam?.Href} target="_blank"><i className="ico steam-ico" /></a>
+    <a href={'https://bazar.lowcygier.pl/' + rowData.Bazarek?.Href} target="_blank"><i className="ico bazarek-ico" /></a>
   </div>;
 };
